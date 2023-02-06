5b95e4d8a4efe346e1b98f1834caf831d4af8073
Marcelinhoj <m_seoy@msn.com>
2023-02-06 20:57:23 -0300

    1

--- Function/function1.js
+++ Function/function1.js
@@ -7,5 +7,15 @@ function createPhrases () {
   console.log('Estudar é muito bom')
   console.log('Paciência e persistencia')
   console.log('Revisão é mãe do aprendizado')
-
 }
+
+// executar a função
+// rodar, chamar , invocar
+// execute, run , call , invoke
+
+createPharases()
+createPharases()
+createPharases()
+
+createPharases()
+console.log('Fim do programa')
