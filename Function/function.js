d673c6d57e789d59c8651fb8dd806527371a52b5
Marcelinhoj <m_seoy@msn.com>
2023-02-06 20:43:05 -0300

    1

---
+++ Function/function.js
@@ -0,0 +1,11 @@
+// criar um aplicativo de frases motivacionais
+
+//declaration -mdeclaração da função
+// function statement
+
+function createPhrases () {
+  console.log('Estudar é muito bom')
+  console.log('Paciência e persistencia')
+  console.log('Revisão é mãe do aprendizado')
+
+}
