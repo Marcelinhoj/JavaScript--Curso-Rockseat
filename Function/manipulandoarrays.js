60ed7d53ec5c05d2e78e5e7b8c85d36bb90e12c7
Marcelinhoj <m_seoy@msn.com>
2023-02-07 21:42:38 -0300

    1

--- Function/manipulandoarrays.js
+++ Function/manipulandoarrays.js
@@ -1,1 +1,14 @@
 // Manipulando Arrays
+
+let techs = ["html", "css", "js"]
+
+// adicionar um intem no fim
+techs.unshift("sql")
+// adicionar no começo
+// remover do fim
+// remover do começo
+// pegar somente alguns elementos do arrau
+// remover 1 ou mais itens em qualquer posição do array
+// encontrar a posição de um elemento no array
+
+console.log(techs)
