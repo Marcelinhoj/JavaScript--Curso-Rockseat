e2014ecb0561833db4f1a6931e7975eaa052e6f8
Marcelinhoj <m_seoy@msn.com>
2023-02-13 20:31:01 -0300

    1

---
+++ Condicionais e cotrole de fluxo/switch.js
@@ -0,0 +1,26 @@
+// switch
+
+function calculate(number1, operator, number2) {
+  let result 
+
+  switch (operator){
+    case '+':
+      break
+    
+    case '-':
+      break
+
+    case '*':
+      break
+
+    case '/':
+      break
+      
+    default:
+      console.log('Não implementado')
+      break
+  }
+
+  return result
+
+}
