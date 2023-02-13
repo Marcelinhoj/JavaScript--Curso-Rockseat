93fe5c916bb33e88da2169bb697d5c09714fe342
Marcelinhoj <m_seoy@msn.com>
2023-02-13 20:21:37 -0300

    1

---
+++ Condicionais e cotrole de fluxo/if_e_else.js
@@ -0,0 +1,12 @@
+// if e else
+
+let temperature = 38
+
+
+if(temperature >=37.5) {
+  console.log( 'Febre alta')
+} else if(temperature < 37.5 && temperature >= 37) {
+  console.log('Febre moderada')
+} else{
+  console.log('Saudável')
+}
