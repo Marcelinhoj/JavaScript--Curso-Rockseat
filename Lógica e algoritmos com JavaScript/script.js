3c5315edf154eea4475b9525dbec427fce5a4c9b
Marcelinhoj <m_seoy@msn.com>
2023-02-20 16:07:35 -0300

    1

---
+++ Lógica e algoritmos com JavaScript/script.js
@@ -0,0 +1,44 @@
+/*
+  Crie um lista de pacientes
+
+  Cada paciente dentro da lista, deverá conter
+    nome
+    idadade
+    peso
+    altura
+
+  Escreva uma lista contendo o nome dos pacientes
+
+*/
+
+const patients = [
+  {
+    name: "Luiz",
+    age : 20,
+    weight: 100,
+    height: 190,
+  },
+  {
+    name: "Alexandra",
+    age : 27,
+    weight: 70,
+    height: 170,
+  },
+  {
+    name: "Carlos",
+    age : 42,
+    weight: 90,
+    height: 180,
+  },
+
+]
+
+let patientsNames = []
+
+for(let patients of patients) {
+  patientsNames.push(patient.name)
+}
+
+alert(patientsNames)
+  
+
