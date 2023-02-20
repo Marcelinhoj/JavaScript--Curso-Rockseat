c71ca41a7278aa571006b8cd7c40263c277492eb
Marcelinhoj <m_seoy@msn.com>
2023-02-20 15:20:46 -0300

    1

--- Lógica e algoritmos com JavaScript/07/script.js
+++ Lógica e algoritmos com JavaScript/07/script.js
@@ -18,39 +18,33 @@
 let option
 let items = []
 
-while(orption !=3) {
-  
+while(option != 3) {
   option = Number(prompt(`
-    Olá usuário! Digite o número da opção desejada
-    
-    1. Cadastrar um tirem na lista
-    2. Mostrar itens cadastrados
-    3. Sair do programa
+  Olá usuário! Digite o número da opção desejada
+
+  1. Cadastrar um item na lista
+  2. Mostrar itens cadastrados
+  3. Sair do programa
   `))
 
-
-  switch(option){
-    case 1:
-      let item = prompt("Digite o nome do item")
-      items.push(item)
-      break
-    
-    case 2:
-      if(items.lengh == 0){
-        alert("Não exitem itens cadastrados")
-      } else {
-        alert(items)
-      }
-      break
-
-    case 3:
-      alert("Tchau")
-      break
-    
-    default:
-      alert("Opção inválida. Tente novamente")
-
+  if(option == 1){
+    let item = prompt("Digite o nome do item")
+    items.push(item)
   }
 
-  
+  else if (option ==2) {
+
+    if( items.length == 0) {
+      alert("Não existe itens cadastrados")
+    } else {
+      alert(items)
+    }
+
+
+  }else {
+    alert("Tchau")
+  }
+
+  console.log(otion, items)
+
 }
---
+++ Lógica e algoritmos com JavaScript/notes.md
@@ -0,0 +1,6 @@
+# Ferramentas da linguagem que abordaremos
+
+[] Loop while
+[] arrays e funções de arrays 
+[] consicional switch e if/else
+[] template literals (strings)
