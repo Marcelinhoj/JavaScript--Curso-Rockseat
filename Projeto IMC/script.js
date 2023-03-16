8e36ff38a2b74427819e43c9fd0f8bbf8bdc7ebd
Marcelinhoj <m_seoy@msn.com>
2023-03-16 06:55:36 -0300

    1

---
+++ Projeto IMC/script.js
@@ -0,0 +1,20 @@
+// variaveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+//3 maneiras de criar e atribuir função a um evento
+
+//form.onsubmit = () => {}
+
+//function handleSubmit () {
+//}
+
+form.onsubmit = function(event) {
+  event.preventDefault() //pra pagina nao atualizar
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+}
+
+
