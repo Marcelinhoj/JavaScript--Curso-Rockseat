d104cb30d4ad1af63a705218e9ec3ce0ae584d9f
Marcelinhoj <m_seoy@msn.com>
2023-03-20 20:08:01 -0300

    1

--- Projeto IMC/script.js
+++ Projeto IMC/script.js
@@ -3,6 +3,11 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClosed = document.querySelector('.modal button.close')
+
+
 //3 maneiras de criar e atribuir função a um evento
 
 //form.onsubmit = () => {}
@@ -10,15 +15,24 @@ const inputHeight = document.querySelector('#height')
 //function handleSubmit () {
 //}
 
-form.onsubmit = function(event) {
+form.onsubmit = event => {
   event.preventDefault() //pra pagina nao atualizar
 
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+  
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
 }
 
+modalBtnClosed.onclick = ()
+ => {
+    modalWrapper.classList.remove('open')
+ }
 function IMC (weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
 }
