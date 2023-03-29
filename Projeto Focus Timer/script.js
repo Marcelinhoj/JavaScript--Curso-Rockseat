5b420b0b864c37b563714d5a44538a942048f944
Marcelinhoj <m_seoy@msn.com>
2023-03-29 19:33:19 -0300

    1

---
+++ Projeto Focus Timer/script.js
@@ -0,0 +1,18 @@
+// Dom
+// Document Object Model
+
+let play = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+
+
+play.addEventListener('click', function() {
+  play.classList.add('hide')
+  
+  pause.classList.remove('hide')
+  
+})
+
+pause.addEventListener('click', function(){
+  pause.classList.add('hide')
+  play.classList.remove('hide')
+})
