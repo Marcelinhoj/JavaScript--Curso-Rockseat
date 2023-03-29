f3bbbe5447bf4a7da039005921b5bb73f13d0b28
Marcelinhoj <m_seoy@msn.com>
2023-03-29 20:55:28 -0300

    1

--- Projeto Focus Timer/script.js
+++ Projeto Focus Timer/script.js
@@ -1,18 +1,91 @@
 // Dom
 // Document Object Model
 
-let play = document.querySelector('.play')
-let pause = document.querySelector('.pause')
+// refatoração: mudar um código para deixá-lo mais entendível
+// deixar o codigo mais performático
+// SEM ALTERAR suas funcionalidades
+
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+let minutes 
+const minutesDisplay = document.querySelector('.minutes')
+const secundsDisplay = document.querySelector('.secunds')
+
+// Event-driven
+// programação imperativa
+// callback
+
+function countdown() {
+  setTimeout(function() {
+    let secunds = Number(secundsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    secundsDisplay.textContent = String(secunds -1).padStart(2, "0")   
+    
+    
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+      
+      return
+    }
+    
+    if ( secunds <= 0) {
+      secunds = 60
+
+      minutesDisplay.textContent = String(minutes -1).padStart(2, "0")
+    }
+
+    secundsDisplay.textContent = String(secunds -1).padStart(2, "0")   
+
+    
+    countdown()
+  }, 1000)
+  
+}
+
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
+
+  countdown()
 
 
-play.addEventListener('click', function() {
-  play.classList.add('hide')
-  
-  pause.classList.remove('hide')
-  
 })
 
-pause.addEventListener('click', function(){
-  pause.classList.add('hide')
-  play.classList.remove('hide')
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+
 })
