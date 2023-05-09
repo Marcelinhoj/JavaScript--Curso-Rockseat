943cfefef93a779883f7efcfbdf12a510ac9080b
Marcelinhoj <m_seoy@msn.com>
2023-05-09 06:41:02 -0300

    1

--- Projeto Focus Timer/JS/index.js
+++ Projeto Focus Timer/JS/index.js
@@ -12,14 +12,16 @@ import Controls from "./controls.js"
 //name import
 import Timer from "./timer.js"
 
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+import { 
+    buttonPlay,
+    buttonPause,
+    buttonSet,
+    buttonSoundOff,
+    buttonSoundOn,
+    minutesDisplay,
+    secondsDisplay
+ } from "./elements.js"
+
 
 const controls = Controls({
   buttonPause,
