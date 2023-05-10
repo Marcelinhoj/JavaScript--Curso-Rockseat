e5890c261506ca26a826d858d3f3d2617afef25f
Marcelinhoj <m_seoy@msn.com>
2023-05-10 06:52:12 -0300

    1

--- Projeto Focus Timer/JS/timer.js
+++ Projeto Focus Timer/JS/timer.js
@@ -1,78 +1,66 @@
-import Sounds from "./sounds"
+import Sounds from "./sounds.js"
 
+export default function Timer({ //dependencias
+    minutesDisplay,
+    secondsDisplay,
+    resetControls,
+}) {
 
-
-export default function Timer({
-  minutesDisplay,
-  secondsDisplay,  
-  resetControls
-  }) {
-
-  let timerTimeOut
-  let minutes = Number(minutesDisplay.textContent)
-
-
-function updateDisplay(newMinutes, seconds) {
-  newMinutes = newMinutes === undefined ? minutes : newMinutes
-  seconds = seconds === undefined ? 0 : seconds
-  minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
-  secondsDisplay.textContent = String(seconds).padStart(2, "0")
-  updateMinutes(minutes)
-}
-
-function reset() {
-  updateDisplay(minutes, 0)
-  clearTimeOut(timerTimeOut)
-}
-
-
-function countdown() {
-  
-   timerTimeOut = setTimeout(function() {
-    let seconds = Number(secondsDisplay.textContent)
+    let timerTimeOut
     let minutes = Number(minutesDisplay.textContent)
-    let isFinished = minutes <= 0 && seconds <= 0
-
-      updateDisplay(minutes, 0) 
     
-    
-    if (isFinished) {     
-      resetControls()
-      updateDisplay()
-      Sounds().timeEnd()   
-      return
+    function updateDisplay(newMinutes, seconds) {
+        newMinutes = newMinutes === undefined ? minutes : newMinutes
+        seconds = seconds === undefined ? 0 : seconds
+        minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
+        secondsDisplay.textContent = String(seconds).padStart(2, "0")//convertendo para string eu consigo com padStart preencher uma casa antes dos numeros 9 ao 1. (2, é o tanto de casas e "0" é a string que quero usar)
     }
     
-    if ( seconds <= 0) {
-      seconds = 60
-      --minutes 
+    function reset() {
+        updateDisplay(minutes, 0)
+        clearTimeout(timerTimeOut)
+    }
+    
+    function countdown() {
+        timerTimeOut = setTimeout(function() {
+            let seconds = Number(secondsDisplay.textContent)
+            let minutes = Number(minutesDisplay.textContent)
+            let isFinished = minutes <= 0 && seconds <= 0
+    
+            updateDisplay(minutes, 0)
+    
+            if (isFinished) {
+                resetControls()
+                updateDisplay() 
+                Sounds().timeEnd()
+                return
+            }
+    
+            if (seconds <= 0) {
+                seconds = 60
+                --minutes
+            }
+        
+            updateDisplay(minutes, String(seconds - 1))
+    
+            countdown()
+        }, 1000)
     }
 
-     
-    updateDisplay(minutes, String(seconds -1))
+
+    function updateMinutes(newMinutes) {
+        minutes = newMinutes
+    }
     
-    countdown()
-  }, 1000)
-  
-}
+    function hold() {
+        clearTimeout(timerTimeOut)
+    }
 
-function updateMinutes (newMinutes) {
-  minutes = newMinutes
-}
-
-function hold() {
-  clearTimeout (timerTimeOut)
-
-}
-
-return {
-  countdown,
-  reset,
-  updateDisplay,
-  updateMinutes,
-  hold
-}
-
-}
-
-
+    return {
+        countdown,
+        reset,
+        updateDisplay,
+        updateMinutes,
+        hold
+    }
+  }
