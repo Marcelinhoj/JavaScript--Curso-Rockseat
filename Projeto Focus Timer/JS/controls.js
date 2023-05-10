50d0ca8bee0b169bca56f7e84404fd5e6f847f3a
Marcelinhoj <m_seoy@msn.com>
2023-05-10 06:47:32 -0300

    1

--- Projeto Focus Timer/JS/controls.js
+++ Projeto Focus Timer/JS/controls.js
@@ -1,49 +1,44 @@
 export default function Controls({
-  buttonPlay,
   buttonPause,
+  buttonPlay,
   buttonSet,
   buttonStop
 }) {
 
-  function play(){
-    buttonPlay.classList.add('hide')
-    buttonPause.classList.remove('hide')    
-    buttonSet.classList.add('hide')
-    buttonStop.classList.remove('hide')
-  
+  function play() {
+      buttonPlay.classList.add('hide')
+      buttonPause.classList.remove('hide')
+      buttonSet.classList.add('hide')
+      buttonStop.classList.remove('hide')
   }
 
-  function pause(){
-    buttonPause.classList.add('hide')
-    buttonPlay.classList.remove('hide')
+  function pause() {
+      buttonPause.classList.add('hide')
+      buttonPlay.classList.remove('hide')
   }
 
-   function reset() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
+  function reset() {
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
   }
 
   function getMinutes() {
-    let newMinutes = prompt('Quantos minutos?')
-    if (!newMinutes) {
-      
+      let newMinutes = prompt('Quantos minutos?')
+      if (!newMinutes) {
       return false
-    }
-  
-    return newMinutes    
-  
+      }
+
+      return newMinutes
   }
 
   return {
-    reset,
-    play,
-    pause,
-    getMinutes
-    
+      reset,
+      play,
+      pause,
+      getMinutes
   }
-
-
 }
-
+Footer
+© 2023 Git
