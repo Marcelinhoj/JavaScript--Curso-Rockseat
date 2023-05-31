90c32fd2fe7038dd45692f48391d1658d6641090
Marcelinhoj <m_seoy@msn.com>
2023-05-31 06:22:57 -0300

    1

--- Projeto-07/js/index.js
+++ Projeto-07/js/index.js
@@ -6,24 +6,8 @@ const routes = {
 }
 
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
 
-  window.history.pushState({}, "", event.target.href)
 
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-  fetch(route)
-  .then(data => data.text ())      
-  .then(html => {
-    document.querySelector('#app').innerHTML = html
-  })
-}
 
   handle()
 
